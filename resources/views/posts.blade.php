82464c8a38c9c2fa5c1de642bf8a752c0f37d5a1
AlanusK <akajara@gmail.com>
2022-04-03 01:22:44 +0300

    Initialize Laravel project

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,26 @@
+<!doctype html>
+
+<title>My Blog</title>
+<link rel="stylesheet" href="/app.css">
+{{-- <script src="/app.js"></script> --}}
+
+
+
+<body>
+
+    <article>
+        <h1><a href="/post">My first post</a></h1>
+        <p>A paragraph is a self-contained unit of discourse in writing dealing with a particular point or idea. A paragraph consists of one or more sentences. Though not required by the syntax of any language. Laravel is a free, open-source PHP web framework, created by Taylor Otwell and intended for the development of web applications following the model–view–controller architectural pattern and based on</p>
+    </article> 
+
+    <article>
+        <h1><a href="/post">My second post</a></h1>
+        <p>A paragraph is a self-contained unit of discourse in writing dealing with a particular point or idea. A paragraph consists of one or more sentences. Though not required by the syntax of any language. Laravel is a free, open-source PHP web framework, created by Taylor Otwell and intended for the development of web applications following the model–view–controller architectural pattern and based on</p>
+    </article>
+
+    <article>
+        <h1><a href="/post">My third post</a></h1>
+        <p>A paragraph is a self-contained unit of discourse in writing dealing with a particular point or idea. A paragraph consists of one or more sentences. Though not required by the syntax of any language. Laravel is a free, open-source PHP web framework, created by Taylor Otwell and intended for the development of web applications following the model–view–controller architectural pattern and based on</p>
+    </article>
+
+</body>
